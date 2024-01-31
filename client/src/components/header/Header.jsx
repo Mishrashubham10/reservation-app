32c035850a2969bd068629c3026d0b7229089496
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-01-31 16:00:31 +0530

    Working with client

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+        <div className="headerList">
+            <div className="headerListItem">
+                
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
