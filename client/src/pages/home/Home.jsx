ff8afa092174e1153d9197b984feb98e17c602d0
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-01-31 17:50:28 +0530

    Create other components for home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Featured from '../../components/featured/Featured';
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
+import PropertyList from '../../components/propertyList/PropertyList';
 import './home.css';
 
 const Home = () => {
@@ -10,6 +11,13 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured />
+        <h1 className="homeTitle">
+          Browse by property type
+        </h1>
+        <PropertyList />
+        <h1 className='homeTitle'>
+          Homes guest love
+        </h1>
       </div>
     </div>
   );
