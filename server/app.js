6729f71d39d2f0130d602c38c161f7d1ddc0fae0
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-02-02 19:53:22 +0530

    Creates routes and endpoints

--- server/app.js
+++ server/app.js
@@ -2,6 +2,12 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+// Routes Import 
+import authRoute from "./routes/auth.route.js";
+import usersRoute from "./routes/users.route.js";
+import hotelsRoute from "./routes/hotels.route.js";
+import roomsRoute from "./routes/rooms.route.js";
+
 const app = express();
 
 // MIDDLEWARE TO HANDLE DATA
@@ -15,5 +21,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
 export default app;
