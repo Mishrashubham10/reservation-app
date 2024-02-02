21f8c11d67cbe785b10d4e68122e1d9955a275fd
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-02-02 20:14:26 +0530

    Creates models and controllers

---
+++ server/controllers/hotels.controller.js
@@ -0,0 +1,18 @@
+import { asyncHandler } from '../utils/asynHandler.js';
+
+// Create Hotel
+const createHotel = asyncHandler(async (req, res) => {});
+
+// UPDATE
+const updateHotel = asyncHandler(async (req, res) => {});
+
+// DELETE
+const deleteHotel = asyncHandler(async (req, res) => {});
+
+// GET
+const hotel = asyncHandler(async (req, res) => {});
+
+// GET ALL
+const hotels = asyncHandler(async (req, res) => {});
+
+export { createHotel, updateHotel, deleteHotel, hotel, hotels };
--- server/routes/hotels.route.js
+++ server/routes/hotels.route.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import {
+  createHotel,
+  deleteHotel,
+  hotel,
+  hotels,
+  updateHotel,
+} from '../controllers/hotels.controller.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello, this is auths end point');
-});
+router.post("/", createHotel);
+router.get("/", hotels);
+router.get("/:id", hotel);
+router.patch("/", updateHotel);
+router.delete("/", deleteHotel);
 
 export default router;
